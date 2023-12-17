a1397150b29cb73eff49d32b6c07e9a6cf831363
Ryan <z5371624@ad.unsw.edu.au>
2023-12-17 15:38:17 +1100

    added nav draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       Hi! My name is Ryan and this is my personal website. I am currently a UNSW student studying computer science with a passion for full-stack development.
     </div>
   );
---
+++ src/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './NavBar.css';
+
+const Navbar = () => {
+    return (
+        <header>       
+            <nav id='navbar'>
+                <div id='navItems'>
+                    <div id='navName'>
+                        <h1>
+                            Ryan Imam
+                        </h1>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
